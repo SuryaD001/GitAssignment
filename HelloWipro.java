3e76187ed694b621877791e76304c388a818dc62
Geetha <danda.saigeetha@wipro.com>
2024-04-21 08:43:56 +0000

    adding java programs

---
+++ HelloWipro.java
@@ -0,0 +1,10 @@
+package com.wipro;
+
+public class HelloWipro
+{
+    public static void main( String[] args )
+    {
+       String str = "Hello Wipro!";
+       System.out.println(str);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.wipro;
+
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+       String str = "Hello World!";
+       System.out.println(str);
+    }
+}
